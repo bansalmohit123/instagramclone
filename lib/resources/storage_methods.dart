f23299583fb7fca81c91254b6babd74c49cee5db
bansalmohit123 <149596867+bansalmohit123@users.noreply.github.com>
2024-08-15 16:27:00 +0530

    feed

--- lib/resources/storage_methods.dart
+++ lib/resources/storage_methods.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:uuid/uuid.dart';
 
 class StorageMethods {
   final FirebaseStorage _storage =FirebaseStorage.instance;
@@ -11,6 +12,12 @@ class StorageMethods {
 
   Future<String> uploadImagetoStorage(String childName ,Uint8List file ,bool ispost)async{
    Reference ref= _storage.ref().child(childName).child(_auth.currentUser!.uid);
+   
+   if(ispost){
+    String id = const Uuid().v1();
+    ref =ref.child(id);
+   }
+
   
    UploadTask uploadTask =  ref.putData(file);
      TaskSnapshot snap= await  uploadTask;
